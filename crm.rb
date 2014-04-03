453e5d4f3fffa6a051465c16223209aee043a633
Brian Simon <tarsipix@gmail.com>
2014-04-03 14:29:04 -0400

    somewhat works

--- crm.rb
+++ crm.rb
@@ -4,6 +4,9 @@ require_relative 'rolodex'
 require 'sinatra'
 
 @@rolodex = Rolodex.new
+@@rolodex.add_contact(Contact.new("Brian", "Simon", "brian.simon@tarsipix.com","Test Note"))
+@@rolodex.add_contact(Contact.new("John", "Doe", "hello.goodbye@snoopy.com","Test Note 2"))
+
 
 get '/' do
   @crm_app_name = "Brian CRM"
@@ -19,6 +22,12 @@ get "/contacts/new" do
   erb :new_contact
 end
 
+get "/contacts/:id" do
+  puts params
+  @contact_id = @@rolodex.find(params[:id])
+  erb :view_contact
+end
+
 get "/contacts/:id/edit" do
 
   # erb :contacts
@@ -27,5 +36,5 @@ end
 post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
   @@rolodex.add_contact(new_contact)
-redirect to('/contacts')
+  redirect to('/contacts')
 end
